06fc5cdc7acfce9016bcf6a9ea040f987a42e412
Dave Spanton <davespanton@gmail.com>
2015-10-21 10:51:24 +0200

    Added an alias for 'current branch'

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -40,6 +40,7 @@ alias gbm='git branch -m'
 alias gbM='git branch -M'
 alias gbs='git show-branch'
 alias gbS='git show-branch -a'
+alias gbh='git rev-parse --abbrev-ref HEAD'
 
 # Commit (c)
 alias gc='git commit --verbose'
